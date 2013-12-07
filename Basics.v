bdd57589ac81639039d5baa038b2e08c8b6854c3
Kei Hibino <ex8k.hibino@gmail.com>
2013-12-07 11:56:15 +0900

    Add newlines.

--- Basics.v
+++ Basics.v
@@ -336,7 +336,11 @@ Admitted.を削除し、証明を完成させなさい。
 Theorem plus_id_exercise : forall n m o : nat,
   n = m -> m = o -> n + m = m + o.
 Proof.
-  intros n m o. intros H0 H1. rewrite -> H0, <- H1. reflexivity. Qed.
+  intros n m o.
+  intros H0 H1.
+  rewrite -> H0, <- H1.
+  reflexivity.
+Qed.
 (* ☐ *)
 
 
