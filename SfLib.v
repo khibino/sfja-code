a307c978433a796f4ce314eac2bffe00b4df594f
Kei Hibino <ex8k.hibino@gmail.com>
2014-08-23 00:55:16 +0900

    Add tactic notations.

--- SfLib.v
+++ SfLib.v
@@ -370,3 +370,20 @@ Lemma extend_shadow : forall A (ctxt: partial_map A) t1 t2 x1 x2,
 Proof with auto.
   intros. unfold extend. destruct (beq_id x2 x1)...
 Qed.
+
+(* 使い勝手のいいタクティックをいくつか *)
+
+Tactic Notation "solve_by_inversion_step" tactic(t) :=
+  match goal with
+  | H : _ |- _ => solve [ inversion H; subst; t ]
+  end
+  (* ⇓ fail "because the goal is not solvable by inversion." *).
+
+Tactic Notation "solve" "by" "inversion" "1" :=
+  solve_by_inversion_step idtac.
+Tactic Notation "solve" "by" "inversion" "2" :=
+  solve_by_inversion_step (solve by inversion 1).
+Tactic Notation "solve" "by" "inversion" "3" :=
+  solve_by_inversion_step (solve by inversion 2).
+Tactic Notation "solve" "by" "inversion" :=
+  solve by inversion 1.
