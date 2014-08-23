302d3de4345b0185eeb5884fdcefac607b292c88
Kei Hibino <ex8k.hibino@gmail.com>
2014-08-23 13:00:15 +0900

    Update about Id.

--- Imp.v
+++ Imp.v
@@ -317,3 +317,85 @@ End AExp.
 (* 変数を持つ式 *)
 
 (* 識別子 *)
+
+Module Id.
+
+Inductive id : Type :=
+  Id : nat -> id.
+
+Definition beq_id X1 X2 :=
+  match (X1, X2) with
+    (Id n1, Id n2) => beq_nat n1 n2
+  end.
+
+Theorem beq_id_refl :
+  forall X, true = beq_id X X.
+Proof.
+  intros. destruct X.
+  apply beq_nat_refl. Qed.
+
+(* 練習問題: ★, optional (beq_id_eq) *)
+
+(*
+この問題とそれに続く練習問題では、帰納法を使わずに、既に証明した自然数の
+同様の結果を適用しなさい。上述したいくつかのタクティックが使えるかもしれ
+ません。
+ *)
+
+Theorem beq_id_eq :
+  forall i1 i2, true = beq_id i1 i2 -> i1 = i2.
+Proof.
+  intros i1 i2 EQ.
+  destruct i1 as [n1].
+  destruct i2 as [n2].
+  unfold beq_id in EQ.
+  apply beq_nat_eq in EQ.
+  rewrite <- EQ.
+  reflexivity.
+Qed.
+(* ☐ *)
+
+(* 練習問題: ★, optional (beq_id_false_not_eq) *)
+
+Theorem beq_id_false_not_eq :
+  forall i1 i2, beq_id i1 i2 = false -> i1 <> i2.
+Proof.
+  intros i1 i2 NE.
+  destruct i1 as [n1].
+  destruct i2 as [n2].
+  unfold beq_id in NE.
+  apply beq_nat_false in NE.
+  intro eq.
+  apply NE.
+  inversion eq as [ID]. (* 自己再帰していないので有り? *)
+  reflexivity.
+Qed.
+(* ☐ *)
+
+(* 練習問題: ★, optional (not_eq_beq_id_false) *)
+
+Theorem not_eq_beq_id_false :
+  forall i1 i2, i1 <> i2 -> beq_id i1 i2 = false.
+Proof.
+  intros i1 i2 NE.
+  destruct i1 as [n1].
+  destruct i2 as [n2].
+  unfold beq_id.
+  apply not_eq_beq_false.
+  intros EQN.
+  apply NE.
+  rewrite <- EQN.
+  reflexivity.
+Qed.
+(* ☐ *)
+
+(* 練習問題: ★, optional (beq_id_sym) *)
+
+Theorem beq_id_sym: ∀ i1 i2,
+  beq_id i1 i2 = beq_id i2 i1.
+Proof.
+Admitted.
+
+(* ☐ *)
+
+End Id.
